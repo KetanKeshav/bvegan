ba1aa9332b07594a08549c3bb52a90d94cccc483
SurajPr92 <surajramesh6292@gmail.com>
2020-09-08 20:19:31 +0530

    chked in

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -40,4 +40,8 @@ export class HeaderComponent implements OnInit {
     // this.userService.authState$.subscribe(authState => this.authState = authState);
   }
 
+  searchProducts() {
+    
+  }
+
 }
